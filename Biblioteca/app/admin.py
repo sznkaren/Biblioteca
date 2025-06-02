de60fd734a1018362dc422ceac1417ef93f1bf04
sznkaren <karen.souza.anderson12@gmail.com>
2025-06-02 08:29:18 -0300

    Add files via upload

--- Biblioteca/app/admin.py
+++ Biblioteca/app/admin.py
@@ -1,11 +1,20 @@
 from django.contrib import admin
+from .models import Cidade, Autor, Editora, Leitor, Livro, Genero
 
-# Register your models here.
-from .models import *
-from django.contrib import admin
 admin.site.register(Cidade)
-admin.site.register(Autor)
 admin.site.register(Editora)
 admin.site.register(Leitor)
-admin.site.register(Livro)
 admin.site.register(Genero)
+
+class LivroInline(admin.TabularInline):
+    model = Livro
+    extra = 1
+
+class AutorAdmin(admin.ModelAdmin):
+    list_display = ('nome',)
+    search_fields = ('nome',)
+    inlines = [LivroInline]
+
+admin.site.register(Autor, AutorAdmin)
+
+admin.site.register(Livro)
